b58dca16e89cb6feb8dabb0fe0163bcb4d0436c2
Berke Öz <beerkeozz@outlook.com>
2025-04-28 22:38:12 +0300

    Görevler tamamlandı

---
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -0,0 +1,51 @@
+package com.workintech.s19d1.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "movie", schema = "fsweb")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "director_name")
+    private String directorName;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH}, fetch =  FetchType.EAGER)
+    @JoinTable(name = "movie_actor", schema = "fsweb",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private List<Actor> actors;
+
+    public void addActor(Actor actor){
+        if(actors == null){
+            actors = new ArrayList<>();
+        }
+        actors.add(actor);
+    }
+}
